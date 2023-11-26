d266befde4447c3178c0674b6c418727047ce480
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-11-26 13:53:49 +0100

    Create main login divs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        LoginComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
+        BrowserAnimationsModule,
     ],
     providers: [
         provideClientHydration()
