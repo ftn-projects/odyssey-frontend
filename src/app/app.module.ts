1686bc748f857fd270ae7663bc3070ff839bab96
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-11-26 13:39:15 +0100

    Add account component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { AccountManagementComponent } from './account-management/account-management.component'
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        AccountManagementComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
+        BrowserAnimationsModule,
+        MatIconModule,
     ],
     providers: [
         provideClientHydration()
