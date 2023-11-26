624bcf403fc2758624d063b0d4b95057c366cca6
Arezinko <arezinamilann@gmail.com>
2023-11-26 13:53:24 +0100

    Create accommodation card component files

---
+++ src/app/accommodation-card/accommodation-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-accommodation-card',
+  templateUrl: './accommodation-card.component.html',
+  styleUrl: './accommodation-card.component.css'
+})
+export class AccommodationCardComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AccommodationCardComponent } from './accommodation-card/accommodation-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        AccommodationCardComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
+        BrowserAnimationsModule,
+        MatIconModule
     ],
     providers: [
         provideClientHydration()
