e717ea2c2f2997c3ed13c425bd1f745b3c82ff38
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-11-26 14:14:05 +0100

    Sort imports in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './infrastructure/material/material.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './infrastructure/material/material.module';
 
 @NgModule({
     declarations: [
